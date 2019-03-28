c7436f0b9f952fe8e5a68cfa3c947198e5e52254
junrikson <junrikson@gmail.com>
2019-03-29 02:28:10 +0700

    Initial commit

---
+++ app/src/main/java/com/junrikson/erppelayaran/MainActivity.java
@@ -0,0 +1,168 @@
+package com.junrikson.erppelayaran;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Toast;
+import android.webkit.WebViewClient;
+
+import com.google.zxing.Result;
+
+import java.util.regex.Pattern;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import static android.Manifest.permission.CAMERA;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private static final int REQUEST_CAMERA = 1;
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e("onCreate", "onCreate");
+
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                Toast.makeText(getApplicationContext(), "Permission already granted", Toast.LENGTH_LONG).show();
+
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    private boolean checkPermission() {
+        return (ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUEST_CAMERA);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                if (grantResults.length > 0) {
+
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted) {
+                        Toast.makeText(getApplicationContext(), "Permission Granted, Now you can access camera", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Permission Denied, You cannot access and camera", Toast.LENGTH_LONG).show();
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(CAMERA)) {
+                                showMessageOKCancel("You need to allow access to both the permissions",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{CAMERA},
+                                                            REQUEST_CAMERA);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new android.support.v7.app.AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                if (mScannerView == null) {
+                    mScannerView = new ZXingScannerView(this);
+                    setContentView(mScannerView);
+                }
+                mScannerView.setResultHandler(this);
+                mScannerView.startCamera();
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mScannerView.stopCamera();
+    }
+    @Override
+    public void handleResult(Result rawResult) {
+
+        final String result = rawResult.getText();
+        String[] code = result.split(Pattern.quote("|"));
+        String url = "";
+
+        switch(code[0])
+        {
+            case "IN" :
+                url = "http://ligita.dyndns.org:188/Invoices/GetFromQRCode?id=" + code[1] + "&code= " + code[2];
+                break;
+            case "CO" :
+                url = "http://ligita.dyndns.org:188/MasterContainers/GetFromQRCode?id=" + code[1] + "&code= " + code[2];
+                break;
+            case "BL" :
+                url = "http://ligita.dyndns.org:188/BLs/GetFromQRCode?id=" + code[1] + "&code= " + code[2];
+                break;
+                default:
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Format Tidak Dikenal")
+                            .setMessage("Format " + result + " Tidak dikenal. Mohon periksa kembali QR Code yang di scan.")
+                            .setCancelable(false)
+                            .show();
+        }
+        if(url != "")
+        {
+            setContentView(R.layout.activity_main);
+
+            WebView browser;
+
+            browser = (WebView)findViewById(R.id.webView);
+            browser.setWebViewClient(new MyBrowser());
+            browser.getSettings().setLoadsImagesAutomatically(true);
+            browser.getSettings().setJavaScriptEnabled(true);
+            browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+            browser.loadUrl(url);
+        }
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
+}
